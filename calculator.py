429070688425f80956ea97ef34a0e936481c9566
manolo-restrepo <manolorestrepo2003@gmail.com>
2024-02-07 13:30:19 -0500

    add substract funtion

--- calculator.py
+++ calculator.py
@@ -4,3 +4,6 @@ def substract(num_1,num_2):
     return num_1-num_2
 def multiplication(num_1,num_2):
     return num_1*num_2
+def division(num_1,num_2):
+    if num_2!=0:
+        return num_1/num_2
